a8ca8d733a7eb41eef74e1d00b7822b7123b637c
Brett <89299506+brett1185@users.noreply.github.com>
2022-11-21 13:35:11 -0500

    Add files via upload

--- findAverage.js
+++ findAverage.js
@@ -1,11 +1,13 @@
-x=[1,3,5,7,20,45]
-let sum=0
-for(var i=0; i<x.length; i++){
-    console.log(x[i])
-    sum = sum +x[i]
+
+
+const findAve = (arr)=>{
+    let sum = 0
+    for(let i=0; i<arr.length; i++){
 
     
+    sum = sum + arr[i]
 }
-ave=sum/x.length
-console.log(sum)
-console.log(ave)
+ave=sum / arr.length
+return ave
+}
+console.log(findAve([1,2,3,4,5]))
